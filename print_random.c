34cf21deed36561a83e16ad3a559213695bd79db
Yongancao <tup90476@temple.edu>
2024-09-01 16:15:59 -0400

    FinalTest

--- print_random.c
+++ print_random.c
@@ -6,27 +6,17 @@
 char* rand_string_alloc(size_t size);
 void rand_string(char *s, size_t size);
 
-int main() {
-    srand(time(NULL));
-
-    printf("Random 7 letter word: %s\n", rand_string_alloc(7));
-    
-    return 0;
-}
-
-//Given Project code
-char* rand_string_alloc(size_t size)
+int main()
 {
-     char *s = malloc(size + 1);
-     if (s) {
-         rand_string(s, size);
-     }
-     return s;
-}
+	int a;
 
-// create the random letters of size
-void rand_string(char *s, size_t size){
-    for (int i = 0; i < size; i++) {
-        s[i] = randchar();
-    }
+	/* seed the randomizer */
+	srand( (unsigned)time(NULL) );
+
+	printf("Today's random word: ");
+	for(a=0;a<7;a++)
+		putchar( randchar() );
+	putchar('\n');
+
+	return(0);
 }
